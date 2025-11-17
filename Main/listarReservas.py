89565e513e98868519953668d3766205f01133f7
Ivan-BarraganDD <ivanbarragandd@gmail.com>
2025-11-17 03:31:21 -0300

    mejoras listado

--- Main/listarReservas.py
+++ Main/listarReservas.py
@@ -2,8 +2,7 @@ from shared.mysql_connection import select
 
 def listarReservas():
     reservas = select(
-            "SELECT * FROM reserva WHERE fecha_check_out >= CURDATE() and not estado  = 'cancelada'"
-        )
+            "SELECT * FROM reserva WHERE estado = 'confirmada'   AND CURDATE() >= DATE(fecha_check_in)   AND CURDATE() <  DATE(fecha_check_out);")
     print("Reservas actuales:\n")
     for reserva in reservas:
-        print(f"• Reserva: {reserva[0]}, Habitación: {reserva[1]}, ID Cliente: {reserva[2]}, Duración [{reserva[6]}, {reserva[7]}], Estado: {reserva[5]}\n")
+        print(f"• Reserva: {reserva[0]}, Habitación: {reserva[1]}, ID Cliente: {reserva[2]}, Duración [{reserva[6]}, {reserva[7]}], Estado: {reserva[4]}\n")
